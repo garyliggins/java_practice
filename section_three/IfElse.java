390bb4963e7df8a9c7ab467591e68db43d55a995
gary <gary@ligginsdevelopment.com>
2022-01-05 18:49:12 -0800

    added more practice

---
+++ section_three/IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int grade = 65;
+
+        if (grade > 50) {
+            System.out.println("you passed");
+        } else {
+            System.out.println("study hard next time");
+        }
+    }
+}
+
+// examples:
+// if (grade > 50) {
+//     System.out.println("you passed")
+// }
